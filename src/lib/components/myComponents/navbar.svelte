732a62604cb5819304cbb2a6840f2f27ad1f6591
impappdavid <impappdavid@gmail.com>
2025-02-07 18:53:13 +0100

    Created the project and the navbar installed all dependecies

---
+++ src/lib/components/myComponents/navbar.svelte
@@ -0,0 +1,50 @@
+<script>
+    import Button from "../ui/button/button.svelte";
+</script>
+
+<nav class="flex w-full p-4 justify-between items-center fixed top-0">
+    <div class="flex items-center w-full max-w-24">
+        <a
+            href="../"
+            class="bg-[#df2623] hover:bg-[#df2623cc] px-2 py-0.5 font-bold text-white text-lg rounded-md transition-all font-logo"
+            >AZURA</a
+        >
+    </div>
+    <div class="flex items-center">
+        <a
+            href="../"
+            class="font-medium font-body text-sm px-3 hover:bg-white hover:text-black text-white  py-1.5 rounded-full transition-all"
+            >ABOUT</a
+        >
+        <a
+            href="../"
+            class="font-medium font-body text-sm px-3 hover:bg-white hover:text-black text-white py-1.5 rounded-full transition-all"
+            >AZUROX</a
+        >
+        <a
+            href="../"
+            class="font-medium font-body text-sm px-3 hover:bg-white hover:text-black text-white py-1.5 rounded-full transition-all"
+            >SOCIALS</a
+        >
+        <a
+            href="../"
+            class="font-medium font-body text-sm px-3 hover:bg-white hover:text-black text-white py-1.5 rounded-full transition-all"
+            >MARKET</a
+        >
+        <a
+            href="../"
+            class="font-medium font-body text-sm px-3 hover:bg-white hover:text-black text-white py-1.5 rounded-full transition-all"
+            >GENERATE</a
+        >
+    </div>
+    <div class="flex items-center ">
+        <a
+            href="../"
+            class="bg-[#ab9ff2] hover:bg-[#9587e0] px-3 py-1 font-medium text-white rounded-md font-body flex gap-1 items-center transition-all"
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5.13 19.2c2.297 0 4.023-1.92 5.053-3.436a2.9 2.9 0 0 0-.195.994c0 .885.53 1.516 1.574 1.516c1.433 0 2.965-1.208 3.758-2.51a2 2 0 0 0-.083.524c0 .617.362 1.006 1.1 1.006c2.324 0 4.663-3.959 4.663-7.421C21 7.175 19.58 4.8 16.016 4.8C9.752 4.8 3 12.154 3 16.905C3 18.771 4.044 19.2 5.13 19.2m8.729-9.622c0-.671.39-1.141.96-1.141c.557 0 .947.47.947 1.14c0 .672-.39 1.155-.947 1.155c-.57 0-.96-.483-.96-1.154m2.979 0c0-.671.39-1.141.96-1.141c.557 0 .947.47.947 1.14c0 .672-.39 1.155-.947 1.155c-.57 0-.96-.483-.96-1.154"/></svg>
+            Connect
+            </a
+        >
+    </div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "$lib/components/myComponents/navbar.svelte";
+</script>
+
+<div class="bg-[#eaebe5] h-screen">
+    <Navbar />
+    <section class="w-full h-screen bg-black">
+
+    </section>
+</div>
